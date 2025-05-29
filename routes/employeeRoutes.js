75b8a3e162bbe2f7f2388c44893bb0a66670adb9
FAHIMTIME <fahm.uddin@yahoo.com>
2025-05-29 06:24:52 +0500

    Employe routes: Add route to get employee by ID

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router();
 const Employee = require('../models/employee')
+const leaves = require('../models/leaves');
+
+// ----------------------- EMPLOYEE ROUTES ----------------------- 
 
 //Read all employee
 router.get('/', async (req,res)=>{
@@ -60,5 +63,69 @@ router.put('/:id', async(req, res)=>{
         res.status(500).json({success: false,  msg: "Error while deleting employee"})
     }
 })
+
+
+// ----------------------- LEAVE ROUTES ----------------------- 
+
+//Read all Leaves
+router.get('/', async (req,res)=>{
+    try{
+        const leaves = await leaves.find({isdeleted: false})
+        const response = {success: true, data: leaves, msg: "Data Received"}
+        res.json(response)
+    }
+    catch(err){
+        const response = {success: false, data: [],msg: "Error while reading"}
+        res.status(500).json(response)
+    }
+})
+
+
+//Read single Leave
+router.get('/:id', async (req,res)=>{
+    try{
+        const Leave = await leaves.find(req.params.id)
+        if(!leaves || leaves.isdeleted) return res.status(404).json({success: false, msg: "Leave Not Found"})
+        res.json({sucess: true, data: leaves})
+    }
+    catch(err){
+        const response = {success: false, data: [],msg: "Error while reading single Leave"}
+        res.status(500).json(response)
+    }
+})
+
+//Add new Leave
+router.post('/', async (req,res)=>{
+    try{
+        const newLeave = new leaves(req.body);
+        const saveLeave = await newLeave.save();
+         res.status(200).json({success: true,  msg: "new Leave saved"})
+    }
+    catch(err){
+        res.status(500).json({success: false,  msg: "Error while adding new Leave"})
+    }
+})
+
+router.delete('/:id', async(req, res)=>{
+    try{
+        const delLeave = await leaves.findOneAndUpdate(req.params.id, {isdeleted: true})
+         res.status(200).json({success: true,  msg: "Leave Deleted"})
+    }
+    catch(err){
+        res.status(500).json({success: false,  msg: "Error while deleting Leave"})
+    }
+})
+
+router.put('/:id', async(req, res)=>{
+    try{
+        const deleave = await Employee.findOneAndUpdate(req.params.id, req.body)
+         res.status(200).json({success: true,  msg: "Leave Deleted"})
+    }
+    catch(err){
+        res.status(500).json({success: false,  msg: "Error while deleting Leave"})
+    }
+})
+
+
 
 module.exports = router
