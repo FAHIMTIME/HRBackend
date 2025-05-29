903a9ebe243b4f225865d31ab0d3b91ea40d6676
FAHIMTIME <fahm.uddin@yahoo.com>
2025-05-29 06:21:24 +0500

    Saved in Morning 29-05 6:15 am
    Unclear:
    Employee, employee use
    employee reference in leaves
    use of Id : is it object ID or empnumber

---
+++ models/leaves.js
@@ -0,0 +1,16 @@
+const mongo = require('mongoose');
+const employee = require('./employee');
+
+const leaves = new mongo.Schema({
+    empnumber: String,
+    leaveType: String,
+    dateFrom: String,
+    dateTo: String,
+    reason: String,
+    Approved: {type:Boolean,default:false},
+    employee: { type: mongo.Schema.Types.ObjectId, ref: 'Employee' },
+    isactive: {type: Boolean, default: true},
+    isdeleted: {type: Boolean, default: false}
+}); 
+
+module.exports = mongo.model('leaves', leaves)
